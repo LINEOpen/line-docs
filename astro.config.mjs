57153957e2a992d2deddf1828106a9cf563ef53b
Shotaro Nakamura <syoutarou1103@outlook.jp>
2023-10-15 03:16:13 +0000

    Change to Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-import solidJs from "@astrojs/solid-js";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), svelte()]
 });
